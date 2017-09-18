8c2a31678421a649c2e881ef950c39a1d79f3012
Danail Borisov <danail.borisov1989@gmail.com>
2017-09-17 23:14:52 -0700

    test commit 2

---
+++ script.js
@@ -0,0 +1,11 @@
+/*  var schools = ["CEG DEALER SCHOOL", "PCI DEALER SCHOOL","CASINO GAMING SCHOOL"];
+
+function pickSchool() {
+    var random = Math.floor(Math.random() * 4);
+    return schools[random];
+}  */
+if (jQuery) {
+    console.log("jQUery connected");
+} else {
+    console.log("not connected jQuery");
+}
