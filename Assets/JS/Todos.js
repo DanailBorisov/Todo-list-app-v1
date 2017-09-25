456c789cd06f1c3d3cc8bdac35f444c05e3e30ec
Danail Borisov <danail.borisov1989@gmail.com>
2017-09-25 00:30:42 -0700

    simple Todo app completed

--- Assets/JS/Todos.js
+++ Assets/JS/Todos.js
@@ -1,11 +1,23 @@
-$("li").click(function() {
+$("ul").on("click", "li", function() {
     $(this).toggleClass("completed");
 });
 
 //delete by clicking on the span / X
-$("span").click(function(e) {
+$("ul").on("click", "span", function(e) {
     $(this).parent().fadeOut(500, function() {
         $(this).remove();
     });
     e.stopPropagation();
-})
+});
+$("input[type='text']").keypress(function(event) {
+    if (event.which === 13) {
+        var toDoText = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + toDoText + "</li>");
+
+    }
+
+});
+$(".fa-plus-circle").click(function() {
+    $("input").fadeToggle();
+});
