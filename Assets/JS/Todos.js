3f5c758a959aa410dad84fac871590bb36df8e67
Danail Borisov <danail.borisov1989@gmail.com>
2017-09-24 23:06:26 -0700

    First Push

--- Assets/JS/Todos.js
+++ Assets/JS/Todos.js
@@ -1,3 +1,11 @@
 $("li").click(function() {
     $(this).toggleClass("completed");
 });
+
+//delete by clicking on the span / X
+$("span").click(function(e) {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    });
+    e.stopPropagation();
+})
